fa9b357237ea88204647cd16b5d22fa13612d532
Saleh Rezq <salehrezq@gmail.com>
2020-06-06 00:01:21 +0200

    Start implementation
    Lesson:
    08. A Real App The Burger Builder (Basic Version)\
    09. Starting Implementation of The Burger Builder Container

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import '../../hoc/Auxiliary';
 import Auxiliary from "../../hoc/Auxiliary";
+import classes from './Layout.module.css';
 
 const Layout = props => (
    <Auxiliary>
       <div>Toolbar, SideDrawe, Backdrop</div>
-      <main>
+      <main className={classes.Content}>
          {props.children}
       </main>
    </Auxiliary>
