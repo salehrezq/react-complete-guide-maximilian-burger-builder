252336851f276b870a62a9f6e7ae6639a67b8d54
Saleh Rezq <salehrezq@gmail.com>
2020-06-15 19:43:06 +0200

    Toolbar comp initial implementation, and placement
    Lesson:
    08. A Real App The Burger Builder (Basic Version)\
    27. Adding a Toolbar

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,10 +2,11 @@ import React from 'react';
 import '../../hoc/Auxiliary';
 import Auxiliary from "../../hoc/Auxiliary";
 import classes from './Layout.module.css';
+import Toolbar from "../Navigation/Toolbar/Toolbar";
 
 const Layout = props => (
    <Auxiliary>
-      <div>Toolbar, SideDrawe, Backdrop</div>
+      <Toolbar/>
       <main className={classes.Content}>
          {props.children}
       </main>
