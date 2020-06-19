42fea69cb086f12c20a8ea1032c0b1e7fccd21af
Saleh Rezq <salehrezq@gmail.com>
2020-06-19 08:50:33 +0200

    Reusing the Backdrop component
    Lesson:
    08. A Real App The Burger Builder (Basic Version)\
    33. Reusing the Backdrop

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,19 +1,35 @@
-import React from 'react';
+import React, {Component} from 'react';
 import '../../hoc/Auxiliary';
 import Auxiliary from "../../hoc/Auxiliary";
 import classes from './Layout.module.css';
 import Toolbar from "../Navigation/Toolbar/Toolbar";
 import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
 
-const Layout = props => (
-   <Auxiliary>
-      <Toolbar/>
-      <SideDrawer/>
-      <main className={classes.Content}>
-         {props.children}
-      </main>
-   </Auxiliary>
+class Layout extends Component {
 
-);
+   state = {
+      showSideDrawer: true
+   }
+
+   sideDrawerClosedHandler = () => {
+      this.setState({showSideDrawer: false});
+   }
+
+   render() {
+      return (
+         <Auxiliary>
+            <Toolbar/>
+            <SideDrawer
+               sideDrawerclosed={this.sideDrawerClosedHandler}
+               show={this.state.showSideDrawer}
+            />
+            <main className={classes.Content}>
+               {this.props.children}
+            </main>
+         </Auxiliary>
+      );
+   }
+
+}
 
 export default Layout;
--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -2,18 +2,28 @@ import React from 'react';
 import classes from './SideDrawer.module.css';
 import Logo from "../../Logo/Logo";
 import NavigationItems from "../NavigationItems/NavigationItems";
+import Backdrop from "../../UI/Backdrop/Backdrop";
+import Aux from "../../../hoc/Auxiliary";
 
 const SideDrawer = props => {
 
+   let attachedClasses = [classes.SideDrawer, classes.Close];
+   if (props.show) {
+      attachedClasses = [classes.SideDrawer, classes.Open];
+   }
+
    return (
-      <div className={classes.SideDrawer}>
-         <div className={classes.Logo}>
-            <Logo/>
+      <Aux>
+         <Backdrop show={props.show} clicked={props.sideDrawerclosed}/>
+         <div className={attachedClasses.join(" ")}>
+            <div className={classes.Logo}>
+               <Logo/>
+            </div>
+            <nav>
+               <NavigationItems/>
+            </nav>
          </div>
-         <nav>
-            <NavigationItems/>
-         </nav>
-      </div>
+      </Aux>
    );
 };
 
